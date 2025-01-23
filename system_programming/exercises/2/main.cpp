0d5fb31a12d89f656b47b20f53ffdf883754001c
trieu.phan <pntrieu200799@gmail.com>
2025-01-23 17:00:57 +0700

    updated

--- system_programming/exercises/2/main.cpp
+++ system_programming/exercises/2/main.cpp
@@ -3,3 +3,145 @@
     directory and merge them all to one txt file and returns a file descriptor for the
     new file.
  */
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/file.h>
+#include <vector>
+#include <dirent.h>
+#include <sys/dir.h>
+#include <string.h>
+
+
+#define BUFFER_SIZE 255
+#define SUCCESS 0
+#define ERROR_SYSTEM -1
+#define ERROR_INITIAL -2
+#define DEFAULT_DIR "docs"
+
+/*
+    Reading current directory to find the txt file
+    Reading content fron txt files
+*/
+
+DIR* dir_fd;
+std::vector<char*> doc_files;
+std::vector<int> fds;
+char doc_name[5] = ".txt";
+char out_file[] = "out.txt";
+int out_fd;
+bool init = false;
+
+void init_files() {
+    if ( creat(out_file, S_IREAD | S_IWRITE) == -1 ) {
+        perror("create file failed");
+        return;
+    }
+    dir_fd = opendir(DEFAULT_DIR);
+    init = true;
+}
+
+int getting_files() {
+    if (!init) return ERROR_INITIAL;
+
+    struct dirent* entry = readdir(dir_fd);
+    while (entry != nullptr) {
+        printf("files: %s\n", entry->d_name);
+        // Create pointer and move to the *.txt position
+        char* f_ptr = &entry->d_name[0];
+        f_ptr = f_ptr + (strlen(entry->d_name) - 4);
+        if ( strncmp(f_ptr, doc_name, 4) == 0 ) {
+            doc_files.push_back(entry->d_name);
+        }
+        entry = readdir(dir_fd);
+    }
+
+    return SUCCESS;
+}
+
+int read_files() {
+    if (!init) return ERROR_INITIAL;
+
+    out_fd = open(out_file, O_CREAT | O_RDWR);
+
+    if (out_fd == ERROR_SYSTEM) {
+        perror("Could not open file");
+        return ERROR_SYSTEM;
+    }
+
+    if (doc_files.empty()) {
+        puts(".txt files is empty");
+        return ERROR_SYSTEM;
+    }
+
+    // Reading content from files
+    for (auto f : doc_files) {
+        char file_path[255] = "docs/";
+        strcat(file_path, f);
+        printf("handle file: %s\n", file_path);
+
+        int fd = open(file_path, O_RDONLY);
+        if (fd == ERROR_SYSTEM) {
+            printf("could not open %s\n", f);
+            continue;
+        }
+
+        char buffer[BUFFER_SIZE];
+        memset(buffer,'\0', BUFFER_SIZE);
+        size_t s = read(fd, buffer, BUFFER_SIZE);
+
+        while( s > 0 ) {
+            printf("buffer: %s\n", buffer);
+            write(out_fd, buffer, s);
+            // move to s bytes
+            lseek(fd, s, 1);
+            s = read(fd, buffer, BUFFER_SIZE);
+        }
+        write(out_fd,"\n",1);
+        close(fd);
+    }
+
+    return out_fd;
+}
+
+void release() {
+    close(out_fd);
+    closedir(dir_fd);
+}
+
+// finding file in dir
+int search_file(char* file, DIR* dir = nullptr) {
+    if (!init) return ERROR_INITIAL;
+
+    DIR* internal_dir = dir == nullptr ? dir_fd : dir;
+
+    if (internal_dir == nullptr) {
+        return ERROR_SYSTEM;
+    }
+
+
+    struct dirent* entry = readdir(internal_dir);
+    while (entry != NULL) {
+        if (
+           strlen(entry->d_name) == strlen(file) && (strcmp(entry->d_name, file) == 0)
+        ) {
+            return SUCCESS; /* return success */
+        }
+        entry = readdir(internal_dir);
+    }
+
+    // release internal pointer
+    internal_dir = nullptr;
+
+    return ERROR_SYSTEM;
+}
+
+
+int main() {
+    init_files();
+    getting_files();
+    read_files();
+    release();
+    return 0;
+}
--- system_programming/exercises/3/main.cpp
+++ system_programming/exercises/3/main.cpp
@@ -3,3 +3,117 @@
     begins with that name in the current directory and its sub directories. For example
     > ./a.out foo will return all file names that begins with foo.
 */
+
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/file.h>
+#include <vector>
+#include <dirent.h>
+#include <sys/dir.h>
+#include <string.h>
+#include <memory>
+
+#define MAX_PARAMS 10
+#define SUCCESS 0
+#define FAILED -1
+#define ERROR_INITAL -2
+#define ERROR_ARGUMENT -3
+#define SIZE_BUFFER 255
+
+#define CURRENT_DIR "."
+
+char* input_params[MAX_PARAMS];
+
+int params_parsing(int argc, char* argv[]) {
+    if (argc < 2) {
+        return ERROR_ARGUMENT;
+    }
+    // Initialize as nullptr
+    for (int i = 0; i < MAX_PARAMS; ++i) {
+        input_params[i] = nullptr;
+    }
+    // Parsing argument
+    for (int i = 0; i < argc; ++i) {
+        // allocated memory
+        input_params[i] = new char[SIZE_BUFFER];
+        // initializing value
+        memset(input_params[i],'\0',SIZE_BUFFER);
+        // parsing value from argv
+        strncpy(input_params[i], argv[i], strlen(argv[i]));
+    }
+    return SUCCESS;
+}
+
+void trace_params_mapping() {
+    for (int i = 0; i < MAX_PARAMS; ++i) {
+        if (input_params[i] != nullptr)
+            printf("params %d -> %s\n", i, input_params[i]);
+    }
+}
+
+enum PARAM_TYPE {
+    DIRECTORY = 1,
+    OPTION,
+    REGEX
+};
+
+char* get_params(const PARAM_TYPE& T) {
+    return input_params[(int)T];
+}
+
+void do_list_files(const char* dir, const char* regex, bool isSub = false) {
+    if (dir == nullptr || regex == nullptr) {
+        return;
+    }
+
+    if (isSub && strncmp(dir, ".", 1) == 0) { return; }
+
+    DIR* dir_fd = opendir(dir);
+    if (dir_fd == nullptr) {
+        // fprintf(stdout, "open directory %s failed\n", dir);
+        return;
+    }
+
+    dirent* dir_entry = readdir(dir_fd);
+    while (dir_entry) {
+        // printf("current file: %s\n", dir_entry->d_name);
+        do_list_files(dir_entry->d_name, regex, true);
+
+        if (strncmp(dir_entry->d_name, regex, strlen(regex)) == 0) {
+            printf("%s\n", dir_entry->d_name);
+        }
+        // move to next file
+        dir_entry = readdir(dir_fd);
+    }
+
+    closedir(dir_fd);
+}
+
+void release() {
+    for (int i = 0; i < MAX_PARAMS; ++i) {
+        if (input_params[i]) {
+            delete input_params[i];
+            input_params[i] = nullptr;
+        }
+    }
+}
+
+int main(int argc, char* argv[]) {
+    if (params_parsing(argc, argv) == ERROR_ARGUMENT) {
+        puts("Error in parsing value");
+        return 0;
+    }
+
+    auto get_directory = get_params(PARAM_TYPE::DIRECTORY);
+    auto get_regex = get_params(PARAM_TYPE::REGEX);
+    // printf("get_directory: %s\n", get_directory);
+    // printf("get_regex: %s\n", get_regex);
+
+    do_list_files(get_directory, get_regex);
+
+    release();
+
+    return 0;
+}
