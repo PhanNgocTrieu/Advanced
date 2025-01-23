55410f18a5af5d3d0aee40105919ea0306021f52
trieujohn <pntrieu200799@gmail.com>
2025-01-24 00:06:32 +0700

    updated

--- system_programming/exercises/4/main.cpp
+++ system_programming/exercises/4/main.cpp
@@ -3,3 +3,23 @@
     file type. That is all .txt file in one folder called txt, all .bmp files in another folder
     called bmp etc. The argument to the program is a folder name.
 */
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/file.h>
+#include <vector>
+#include <dirent.h>
+#include <sys/dir.h>
+#include <string.h>
+
+#define BUFFER_SIZE 255
+#define SUCCESS 0
+#define ERROR_SYSTEM -1
+#define ERROR_INITIAL -2
+#define DEFAULT_DIR "docs"
+
+int main(int argc, char* argv[]) {
+    
+    return 0;
+}
