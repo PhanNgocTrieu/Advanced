ca466045767c5c4fa577077aae977fe6a3d4f633
Trieu John <pntrieu200799@gmail.com>
2025-02-27 17:52:24 +0700

    Create main.cpp

---
+++ projects/ls/main.cpp
@@ -0,0 +1,15 @@
+#include "ls_command.h"
+
+void helper() {
+    printf("Usage: ls -d <directory> -f <files> \
+        \n\t-d <directory> : directory to parse \
+        \n\t-f <files> : pattern to catagorize the tail's file \
+    ");
+}
+
+int main(int argc, char *argv[]) {
+
+    ls_command ls;
+
+    return 0;
+}
